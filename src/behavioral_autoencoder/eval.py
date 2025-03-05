4d8ade96913ff106729dfd35373446654d237aef
Ubuntu <ubuntu@ip-172-31-24-130.us-west-2.compute.internal>
2025-03-05 08:18:30 +0000

    added back mean image when predicting

--- src/behavioral_autoencoder/eval.py
+++ src/behavioral_autoencoder/eval.py
@@ -17,11 +17,11 @@ def get_all_predicts_latents(trained_model,datamodule,batch_size,num_workers):
     latents = []
     for batch in tqdm(full_dataloader):
         prediction,latent = trained_model(batch)
-        predictions.append(prediction)
+        predictions.append(prediction+full_dataloader.mean_image[None,None,:])
         latents.append(latent)
     return torch.concatenate(predictions), torch.concatenate(latents)    
 
-def get_dl_predicts_latents(trained_model,dataloader,batch_size,num_workers):
+def get_dl_predicts_latents(trained_model,dataloader,mean,batch_size,num_workers):
     """
     Get latents for the entire video, having trained on a subsampled set. 
 
@@ -30,6 +30,6 @@ def get_dl_predicts_latents(trained_model,dataloader,batch_size,num_workers):
     latents = []
     for batch in tqdm(dataloader):
         prediction,latent = trained_model(batch)
-        predictions.append(prediction)
+        predictions.append(prediction+mean[None,None,:])
         latents.append(latent)
     return torch.concatenate(predictions), torch.concatenate(latents)    
