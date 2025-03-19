658e3ce3e01945da547bdcd0bdccabf140e45649
Taiga Abe <taigaabe@Taigas-MacBook-Air.local>
2025-03-19 12:32:59 -0700

    started building stub of eval

---
+++ scripts/eval_single_session.py
@@ -0,0 +1,5 @@
+"""
+Evaluate a single session autoencoder. 
+
+Assumes that we are given a path to a directory `preds/{modeltype}/date/time/`. Will use the configuration parameters stored there 
+"""
